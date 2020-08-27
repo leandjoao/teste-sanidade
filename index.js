665cf0c1175e0a90bf8c9864ae05f957ac4790ab
leandjoao <leand.joao@outlook.com.br>
2020-08-27 20:26:32 -0300

    Impares

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Import stylesheets
-import './style.css';
+const app = document.querySelector("#app")
+let vetor = []
+let impares = []
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+for(let i = 1; i<=24; i++) {
+  vetor.push(i)
+}
+
+const recebeAula = aula => {
+  aula.forEach(horas => horas % 2 !== 0 ? impares.push(horas): '')
+  return impares
+}
+
+app.innerHTML = recebeAula(vetor)
